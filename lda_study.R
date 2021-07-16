4b7708a51847a8881b404a1109b791af8dbb9cfe
Cherrlyy <ind07229@naver.com>
2021-07-16 22:36:07 +0900

    clustering for DATA

--- lda_study.R
+++ lda_study.R
@@ -20,14 +20,7 @@ df_vaccine = read.any("0704인구수대비접종률데이터.csv", header=T)
 df_vaccine = df_vaccine[, c(3, 4)]
 df_vaccine
 
-install.packages("stats")
-library(stats)
-km = kmeans(df_vaccine, sqrt(18/2))
-km
 
-km$centers
-
-install.packages("factoextra")
 library(factoextra)
 fviz_cluster(km, data = df_vaccine$접종률, stand = F)
 
@@ -52,9 +45,9 @@ nw = v2_group %>% select(-X) %>% group_by(유입대상지역) %>% summarise(sum(
 colnames(nw) = c("행정기관", "유입인구")
 nw
 
-
+#newData = 행정기관, 감염자수, 총인구수, 감염률, 유입인구
 newData = merge(corona, nw)
-
+newData
 
 cor.test(newData$"감염률", newData$"유입인구")
 #p-value = 0.144, cor = 0.3697855이므로 관련 없음
@@ -63,15 +56,66 @@ cor.test(newData$"감염률", newData$"유입인구")
 # 화장실 데이터 가져오기
 toilet = read.any("시도별공공화장실수.csv", header = T)
 toilet = toilet %>% select(-X)
-v2_group = read.any("df_immunisation - df_immunisation.csv", header=T)
-nw = v2_group %>% select(-X) %>% group_by(유입대상지역) %>% summarise(sum(유입지역.접종률))
+
+immun = read.any("df_total_immunisation.csv", header = T)
+immun
+nw = immun %>% select(-X)
+nw
 colnames(nw) = c("행정기관", "접종률")
 
+#newData = 행정기관, 감염자수, 총인구수, 감염률, 유입인구, 화장실수
 newData = merge(newData, toilet)
+newData
+
+#newData = 행정기관, 감염자수, 총인구수, 감염률, 유입인구, 화장실수, 접종률
 newData = merge(newData, nw)
+newData
+
+#clustering 위한 열 선별 및 정규화
 new = newData %>% select(감염률, 화장실수, 접종률)
 new$화장실수 = as.numeric(new$화장실수)
+new$화장실수 = normalize(new$화장실수)
+new
 
+#clustering
 km = kmeans(new, sqrt(18/2))
 plot(new, col = km$cluster)
 
+
+#clustering plot
+y_kmeans = km$cluster
+clusplot(new[, c("감염률", "화장실수")],
+         y_kmeans,
+         lines = 0,
+         shade = TRUE,
+         color = TRUE,
+         labels = 2,
+         plotchar = FALSE,
+         span = TRUE,
+         main = paste("Cluster km"),
+         xlab = '감염률',
+         ylab = '화장실수')
+
+clusplot(new[, c("감염률", "접종률")],
+         y_kmeans,
+         lines = 0,
+         shade = TRUE,
+         color = TRUE,
+         labels = 2,
+         plotchar = FALSE,
+         span = TRUE,
+         main = paste("Cluster km"),
+         xlab = '감염률',
+         ylab = '접종률')
+
+clusplot(new[, c("화장실수", "접종률")],
+         y_kmeans,
+         lines = 0,
+         shade = TRUE,
+         color = TRUE,
+         labels = 2,
+         plotchar = FALSE,
+         span = TRUE,
+         main = paste("Cluster km"),
+         xlab = '화장실수',
+         ylab = '접종률')
